fcd9b5f08e65df67888c01aa64b521d74faa50ad
Spencer Norman <spencern@gmail.com>
2015-11-03 15:45:19 -0700

    Add defaults for shop buffer

--- common/schemas/rentalShops.js
+++ common/schemas/rentalShops.js
@@ -2,15 +2,18 @@ ReactionCore.Schemas.RentalShop = new SimpleSchema([
   ReactionCore.Schemas.Shop, {
     rentalShippingBuffer: {
       type: Number,
-      optional: true
+      optional: true,
+      defaultValue: 0
     },
     rentalReturnBuffer: {
       type: Number,
-      optional: true
+      optional: true,
+      defaultValue: 0
     },
     rentalShippingBufferExclusionZipCodes: {
       type: [String],
-      optional: true
+      optional: true,
+      defaultValue: []
     }
   }
 ]);
