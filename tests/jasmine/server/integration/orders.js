cd28204e8cdcc7a07c7e8c522fa69441d616c809
Spencer Norman <spencern@gmail.com>
2016-04-08 03:13:05 -0600

    position based date reservation tests

--- tests/jasmine/server/integration/orders.js
+++ tests/jasmine/server/integration/orders.js
@@ -5,43 +5,36 @@ describe("getoutfitted:reaction-rental-products orders methods", function () {
   let rentalVariant;
   let inventoryQuantity;
 
-  beforeAll(() => {
-    // this is needed for `inventory/register`
-    spyOn(ReactionCore, "hasPermission").and.returnValue(true);
-    InventoryVariants.remove({});
-    Products = ReactionCore.Collections.Products;
-    product = faker.reaction.products.add();
-    productId = product._id;
-    variant = ReactionCore.Collections.Products.findOne({
-      ancestors: [productId]
-    });
-    variantId = variant._id;
-    rentalProduct = Factory.create("rentalProduct");
-    rentalVariant = Factory.create("rentalVariant", {
-      ancestors: [rentalProduct._id]
-    });
-    inventoryQuantity = rentalVariant.inventoryQuantity;
-    _(inventoryQuantity).times(function (n) {
-      Factory.create("inventoryVariant", {
-        productId: rentalVariant._id,
-        barcode: "BARCODE" + n,
-        sku: "SKU123"
-      });
-    });
-    // give all inventoryVariants variable amounts of booked dates between 30 and 60 days out
-    const existingInventory = InventoryVariants.find({productId: rentalVariant._id}).fetch();
-    existingInventory.forEach(function (inv) {
-      const tmpDaysUntil = _.random(30, 60);
-      const tmpRentalLength = _.random(1, 29);
-      Meteor.call("rentalProducts/reserveInventoryVariantForDates", inv._id, {
-        startTime: moment().startOf("day").add(tmpDaysUntil, "days").toDate(),
-        endTime: moment().startOf("day").add(tmpDaysUntil + tmpRentalLength, "days").toDate()
-      });
-    });
-  });
-
   describe("rentalProducts/inventoryAdjust", function () {
-    it("should reserve dates requested", function (done) {
+    beforeAll(() => {
+      // this is needed for `inventory/register`
+      spyOn(ReactionCore, "hasPermission").and.returnValue(true);
+      InventoryVariants.remove({});
+      rentalProduct = Factory.create("rentalProduct");
+      rentalVariant = Factory.create("rentalVariant", {
+        ancestors: [rentalProduct._id]
+      });
+      inventoryQuantity = rentalVariant.inventoryQuantity;
+      _(inventoryQuantity).times(function (n) {
+        Factory.create("inventoryVariant", {
+          productId: rentalVariant._id,
+          barcode: "BARCODE" + n,
+          sku: "SKU123"
+        });
+      });
+      // give all inventoryVariants variable amounts of booked dates between 30 and 60 days out
+      const existingInventory = InventoryVariants.find({productId: rentalVariant._id}).fetch();
+      existingInventory.forEach(function (inv) {
+        const tmpDaysUntil = _.random(30, 60);
+        const tmpRentalLength = _.random(1, 29);
+        Meteor.call("rentalProducts/reserveInventoryVariantForDates", inv._id, {
+          startTime: moment().startOf("day").add(tmpDaysUntil, "days").toDate(),
+          endTime: moment().startOf("day").add(tmpDaysUntil + tmpRentalLength, "days").toDate()
+        });
+      });
+    });
+
+    it("should reserve dates requested if available", function (done) {
       const daysTilRental = _.random(7, 28);
       const rentalLength = _.random(1, 14);
       const quantityRequested = 1;
@@ -73,13 +66,152 @@ describe("getoutfitted:reaction-rental-products orders methods", function () {
       done();
     });
 
+    it("should error if dates requested are not available", function (done) {
+      // Tests
+      done();
+    });
+
     it("should reserve most booked inventory item available", function (done) {
       // This is tested in above test now
       done();
     });
+  });
 
-    it("should insert dates into correct position", function (done) {
-      // TODO: Add Tests
+  describe("rentalProducts/inventoryAdjust - insert dates into correct position", function () {
+    let iter;
+    let firstDays;
+    let firstStart;
+    let firstEnd;
+    let secondStart;
+    let secondEnd;
+
+    beforeAll(() => {
+      // this is needed for `inventory/register`
+      spyOn(ReactionCore, "hasPermission").and.returnValue(true);
+      rentalProduct = Factory.create("rentalProduct");
+      rentalVariant = Factory.create("rentalVariant", {
+        ancestors: [rentalProduct._id],
+        inventoryQuantity: 1
+      });
+      firstDays = [];
+      firstStart = moment().startOf("day").add(5, "days").toDate();
+      firstEnd = moment().startOf("day").add(7, "days").toDate();
+      iter = moment(firstStart).twix(firstEnd, {allDay: true}).iterate("days");
+      while (iter.hasNext()) { firstDays.push(iter.next().toDate()); }
+
+      secondDays = [];
+      secondStart = moment().startOf("day").add(14, "days").toDate();
+      secondEnd = moment().startOf("day").add(16, "days").toDate();
+      iter = moment(secondStart).twix(secondEnd, {allDay: true}).iterate("days");
+      while (iter.hasNext()) { secondDays.push(iter.next().toDate()); }
+    });
+
+    beforeEach(()=> {
+      ReactionCore.Collections.Orders.remove({});
+      InventoryVariants.remove({});
+      inventoryVariant = Factory.create("inventoryVariant", {
+        productId: rentalVariant._id,
+        barcode: "BARCODE",
+        sku: "SKU123",
+        unavailableDates: []
+      });
+      Meteor.call("rentalProducts/reserveInventoryVariantForDates", inventoryVariant._id, {
+        startTime: firstStart,
+        endTime: firstEnd
+      });
+      Meteor.call("rentalProducts/reserveInventoryVariantForDates", inventoryVariant._id, {
+        startTime: secondStart,
+        endTime: secondEnd
+      });
+    });
+
+    it("should insert dates into beginning position when all other dates are after", function (done) {
+      const start = moment().startOf("day").add(1, "days").toDate();
+      const end = moment().startOf("day").add(4, "days").toDate();
+      let days = [];
+      iter = moment(start).twix(end, {allDay: true}).iterate("days");
+      while (iter.hasNext()) { days.push(iter.next().toDate()); }
+
+      const order = Factory.create("rentalOrder", {
+        startTime: start,
+        endTime: end,
+        items: [
+          {
+            _id: faker.random.uuid(),
+            productId: rentalProduct._id,
+            variants: rentalVariant,
+            quantity: 1,
+            functionalType: "rental"
+          }
+        ]
+      });
+      let previouslyBookedDates = InventoryVariants.findOne({}, {sort: {numberOfDatesBooked: -1}}).unavailableDates;
+
+      expect(_.isEqual(firstDays.concat(secondDays), previouslyBookedDates)).toBeTruthy();
+      Meteor.call("rentalProducts/inventoryAdjust", order._id);
+
+      let bookedDates = InventoryVariants.findOne({}, {sort: {numberOfDatesBooked: -1}}).unavailableDates;
+      expect(_.isEqual(days.concat(firstDays.concat(secondDays)), bookedDates)).toBeTruthy();
+      done();
+    });
+
+    it("should insert dates into middle position when other dates are before and after", function (done) {
+      const start = moment().startOf("day").add(8, "days").toDate();
+      const end = moment().startOf("day").add(12, "days").toDate();
+      let days = [];
+      iter = moment(start).twix(end, {allDay: true}).iterate("days");
+      while (iter.hasNext()) { days.push(iter.next().toDate()); }
+
+      const order = Factory.create("rentalOrder", {
+        startTime: start,
+        endTime: end,
+        items: [
+          {
+            _id: faker.random.uuid(),
+            productId: rentalProduct._id,
+            variants: rentalVariant,
+            quantity: 1,
+            functionalType: "rental"
+          }
+        ]
+      });
+      let previouslyBookedDates = InventoryVariants.findOne({}, {sort: {numberOfDatesBooked: -1}}).unavailableDates;
+
+      expect(_.isEqual(firstDays.concat(secondDays), previouslyBookedDates)).toBeTruthy();
+      Meteor.call("rentalProducts/inventoryAdjust", order._id);
+
+      let bookedDates = InventoryVariants.findOne({}, {sort: {numberOfDatesBooked: -1}}).unavailableDates;
+      expect(_.isEqual(firstDays.concat(days.concat(secondDays)), bookedDates)).toBeTruthy();
+      done();
+    });
+
+    it("should insert dates into last position when all other dates are before", function (done) {
+      const start = moment().startOf("day").add(19, "days").toDate();
+      const end = moment().startOf("day").add(20, "days").toDate();
+      let days = [];
+      iter = moment(start).twix(end, {allDay: true}).iterate("days");
+      while (iter.hasNext()) { days.push(iter.next().toDate()); }
+
+      const order = Factory.create("rentalOrder", {
+        startTime: start,
+        endTime: end,
+        items: [
+          {
+            _id: faker.random.uuid(),
+            productId: rentalProduct._id,
+            variants: rentalVariant,
+            quantity: 1,
+            functionalType: "rental"
+          }
+        ]
+      });
+      let previouslyBookedDates = InventoryVariants.findOne({}, {sort: {numberOfDatesBooked: -1}}).unavailableDates;
+
+      expect(_.isEqual(firstDays.concat(secondDays), previouslyBookedDates)).toBeTruthy();
+      Meteor.call("rentalProducts/inventoryAdjust", order._id);
+
+      let bookedDates = InventoryVariants.findOne({}, {sort: {numberOfDatesBooked: -1}}).unavailableDates;
+      expect(_.isEqual(firstDays.concat(secondDays.concat(days)), bookedDates)).toBeTruthy();
       done();
     });
   });
