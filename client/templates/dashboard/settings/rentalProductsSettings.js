2359cff06345cd861c267baa16c55b9423bcd82c
Spencer Norman <spencern@gmail.com>
2016-04-10 16:38:55 -0600

    init admin UI for rental products

---
+++ client/templates/dashboard/settings/rentalProductsSettings.js
@@ -0,0 +1,22 @@
+Template.rentalProductsSettings.onCreated(function () {
+  this.subscribe("Shops");
+});
+
+Template.rentalProductsSettings.helpers({
+  shop: function () {
+    return ReactionCore.Collections.Shops.findOne();
+  }
+});
+
+AutoForm.hooks({
+  rentalShopEditForm: {
+    onSuccess: function () {
+      return Alerts.add('Shop rental settings saved.', 'success', {
+        autoHide: true
+      });
+    },
+    onError: function (operation, error) {
+      return Alerts.add('Shop rental settings update failed. ' + error, 'danger');
+    }
+  }
+});
--- package.js
+++ package.js
@@ -58,79 +58,29 @@ Package.onUse(function (api) {
 
   // Schemas, Collections, and Hooks
   api.addFiles([
-  "common/common.js",
-  "common/schemas/inventoryVariants.js",
-  "common/schemas/rentalProducts.js", // Schema for rental products
-  "common/schemas/rentalShops.js",
-  "common/schemas/orders.js",
-  "common/schemas/cart.js",
-  "common/collections.js",
-  "common/hooks.js"
-], ["client", "server"]);
+    "common/common.js",
+    "common/schemas/inventoryVariants.js",
+    "common/schemas/rentalProducts.js", // Schema for rental products
+    "common/schemas/rentalShops.js",
+    "common/schemas/orders.js",
+    "common/schemas/cart.js",
+    "common/collections.js",
+    "common/hooks.js"
+  ], ["client", "server"]);
 
-  // helpers
+  // Settings Templates
+  api.addFiles([
+    "client/templates/dashboard/settings/rentalProductsSettings.html",
+    "client/templates/dashboard/settings/rentalProductsSettings.js"
+  ], ["client"]);
 
-  // Product Templates
-  // api.addFiles("client/templates/products/products.html", "client");
-  // api.addFiles("client/templates/products/products.js", "client");
-  //
-  // api.addFiles("client/templates/products/productList/productList.html", "client");
-  // api.addFiles("client/templates/products/productList/productList.js", "client");
-  //
-  // api.addFiles("client/templates/products/productGrid/content/content.html", "client");
-  // api.addFiles("client/templates/products/productGrid/content/content.js", "client");
-  //
-  // api.addFiles("client/templates/products/productGrid/notice/notice.html", "client");
-  // api.addFiles("client/templates/products/productGrid/notice/notice.js", "client");
-  //
-  // api.addFiles("client/templates/products/productGrid/controls/controls.html", "client");
-  // api.addFiles("client/templates/products/productGrid/controls/controls.js", "client");
-  //
-  // api.addFiles("client/templates/products/productGrid/item/item.html", "client");
-  // api.addFiles("client/templates/products/productGrid/item/item.js", "client");
-  //
-  // api.addFiles("client/templates/products/productGrid/productGrid.html", "client");
-  // api.addFiles("client/templates/products/productGrid/productGrid.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/productDetail.html", "client");
-  // api.addFiles("client/templates/products/productDetail/productDetail.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/edit/edit.html", "client");
-  // api.addFiles("client/templates/products/productDetail/edit/edit.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/images/productImageGallery.html", "client");
-  // api.addFiles("client/templates/products/productDetail/images/productImageGallery.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/tags/tags.html", "client");
-  // api.addFiles("client/templates/products/productDetail/tags/tags.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/social/social.html", "client");
-  // api.addFiles("client/templates/products/productDetail/social/social.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/variants/variant.html", "client");
-  // api.addFiles("client/templates/products/productDetail/variants/variant.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/variants/variantList/variantList.html", "client");
-  // api.addFiles("client/templates/products/productDetail/variants/variantList/variantList.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/variants/variantWidget/variantWidget.html", "client");
-  // api.addFiles("client/templates/products/productDetail/variants/variantWidget/variantWidget.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/variants/variantForm/variantForm.html", "client");
-  // api.addFiles("client/templates/products/productDetail/variants/variantForm/variantForm.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/variants/variantForm/childVariant/childVariant.html", "client");
-  // api.addFiles("client/templates/products/productDetail/variants/variantForm/childVariant/childVariant.js", "client");
-  //
-  // api.addFiles("client/templates/products/productDetail/attributes/attributes.html", "client");
-  // api.addFiles("client/templates/products/productDetail/attributes/attributes.js", "client");
-  //
-  // api.addFiles("client/templates/products/productSettings/productSettings.html", "client");
-  // api.addFiles("client/templates/products/productSettings/productSettings.js", "client");
-
-  // Exports
-  // api.export("currentProduct", ["client", "server"]);
-  // api.export("ReactionProduct");
+  // Dashboard Templates
+  api.addFiles([
+    "client/templates/dashboard/rentalProducts/productList.html",
+    "client/templates/dashboard/rentalProducts/productList.js",
+    "client/templates/dashboard/rentalProducts/availability/availability.html",
+    "client/templates/dashboard/rentalProducts/availability/availability.js"
+  ], ["client"]);
 });
 
 Package.onTest(function (api) {
